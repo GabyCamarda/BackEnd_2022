d83d98267c88115bb531140be78b8c9537aa62bb
GCR97 <78401767+GabyCamarda@users.noreply.github.com>
2022-04-04 02:07:21 -0300

    Add files via upload

---
+++ Primer_Desafío/index-resuelto-con-clases-primer-desafío.js
@@ -0,0 +1,47 @@
+// Ejercicios desafio entregable
+
+console.log("***********************Ejercicios desafio entregable***********************");
+
+// 1- 
+class Usuario {
+    // 2-
+    constructor(nombre, apellido, libros, mascotas){
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.libros = libros;
+        this.mascotas = mascotas;
+    }
+
+    // 3-
+    getFullName(){
+        return this.apellido + ", " + this.nombre;
+    }
+
+    addMascota(mascota){
+        this.mascotas.push(mascota);
+        return "mascota agregada " + mascota;
+    }
+
+    countMascotas() {
+        return this.mascotas.length;
+    }
+
+    addBook(nombreLibro, autorLibro){
+        this.libros.push( { nombre: nombreLibro, autor: autorLibro} );
+    }
+
+    getBookNames() {
+        return this.libros.map(libro => libro.nombre);
+    }
+}
+
+let libros = [ { nombre: "The Lord of the rings", autor: "JRR Tolkien"}, { nombre: "The Hobbit", autor: "JRR Tolkien"} ];
+let mascotas = ["Ona", "India"];
+
+// 4-
+let user = new Usuario("Gabriel", "Camarda", libros, mascotas)
+
+console.log("Nombre completo: " + user.getFullName());
+console.log("Agrego mascota: " + user.addMascota("bichito"));
+console.log("Cantidad de mascotas: " + user.countMascotas());
+console.log("Libros: " + user.getBookNames());
